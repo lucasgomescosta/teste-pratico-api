b01d8083c889b93312c577502babea13df864b31
lucas gomes <lucasgomesc582@gmail.com>
2026-04-07 10:41:29 -0400

    feat: estrutura inicial do projeto

---
+++ src/main/java/br/com/teste_pratico_api/TestePraticoApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.teste_pratico_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestePraticoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestePraticoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/teste_pratico_api/TestePraticoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.teste_pratico_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestePraticoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
